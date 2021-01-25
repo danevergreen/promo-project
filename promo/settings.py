11d117ad5c723bb75787913de537c05c2fa97da3
Dmitry <you@example.com>
2021-01-25 15:35:27 +0300

    PostgreSQL

--- promo/settings.py
+++ promo/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'promo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'promdb',
+        'USER': 'postgres',
+        'PASSWORD': 'hfyf22098',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
